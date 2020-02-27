2a4f933fbef40fdfd66ff00247604eb0d3e156c3
jeeyoung <mk0711@uw.edu>
2020-02-26 21:06:18 -0800

    help

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const g = svg.append('g')
 d3.json('nygeo.json').then(function(data) {
 
     d3.csv('data.csv').then(function(airbnbData) {
-       
+
 
         const albersProj = d3.geoAlbers()
             .scale(190000)
@@ -48,35 +48,47 @@ d3.json('nygeo.json').then(function(data) {
             .attr('fill', '#ccc')
             .attr('d', geoPath)
 
+
+        const allPaths = [];
+        const df = data.features;
+        console.log(data.features[1])
+
+            for (let i = 0; i < data.features.length - 1; i++) {
+                const r = albersProj(data.features[i].geometry.coordinates);
+                const n = albersProj(data.features[i+1].geometry.coordinates);
+                allPaths.push([r, n]);
+            }
+        console.log(allPaths)
+
         // plots circles on the boston map
-         g.selectAll('.circle')
-             .data(airbnbData)
-             .enter()
-             .append('circle')
-                 .attr('cx', function(d) { 
-                    let scaledPoints = albersProj([d['longitude'], d['latitude']])
-                     return scaledPoints[0]
-                 })
-                 .attr('cy', function(d) {
-                     let scaledPoints = albersProj([d['longitude'], d['latitude']])
-                    return scaledPoints[1]
-                 })
-                 .attr('r', 5)
-                 .attr('fill', 'steelblue')
-                 .attr("width",60)
-                 .attr("height",50)
-                 .on( "click", function(){
-                   d3.select(this)
-                     .attr("opacity",1)
-                     .transition()
-                     .duration( 1000 )
-                     .attr( "cx", width * Math.round( Math.random() ) )
-                     .attr( "cy", height * Math.round( Math.random() ) )
-                     .attr( "opacity", 0 )
-                     .on("end",function(){
-                       d3.select(this).remove();
-                     })
+        g.selectAll('.circle')
+        .data(airbnbData)
+        .enter()
+        .append('circle')
+            .attr('cx', function(d) { 
+               let scaledPoints = albersProj([d['longitude'], d['latitude']])
+                return scaledPoints[0]
+            })
+            .attr('cy', function(d) {
+                let scaledPoints = albersProj([d['longitude'], d['latitude']])
+               return scaledPoints[1]
+            })
+            .attr('r', 5)
+            .attr('fill', 'steelblue')
+            .attr("width",60)
+            .attr("height",50)
+            .on( "click", function(){
+              d3.select(this)
+                .attr("opacity",1)
+                .transition()
+                .duration( 1000 )
+                .attr( "cx", width * Math.round( Math.random() ) )
+                .attr( "cy", height * Math.round( Math.random() ) )
+                .attr( "opacity", 0 )
+                .on("end",function(){
+                  d3.select(this).remove();
                 })
+           })
         
     })
   
